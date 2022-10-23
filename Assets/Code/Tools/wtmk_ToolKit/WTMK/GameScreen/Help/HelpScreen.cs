7cd3ecd21ffc05387f9c23cf4eee1c6a683ec941
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2022-10-23 08:21:32 -0700

    stub pause

--- Assets/Code/Tools/wtmk_ToolKit/WTMK/GameScreen/Help/HelpScreen.cs
+++ Assets/Code/Tools/wtmk_ToolKit/WTMK/GameScreen/Help/HelpScreen.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class HelpScreen : State<GameState>
 {
+    public override void OnEnter()
+    {
+        _View.SetActive(true);
+    }
     private HelpScreenView _View;
     public HelpScreen(HelpScreenView view, GameState tag) : base(tag)
     {
--- Assets/Code/Tools/wtmk_ToolKit/WTMK/GameScreen/Help/HelpScreenView.cs
+++ Assets/Code/Tools/wtmk_ToolKit/WTMK/GameScreen/Help/HelpScreenView.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class HelpScreenView : MonoBehaviour, IStateView
 {
+    [SerializeField]
+    private GameObject _UI;
     public void SetActive(bool isActive)
     {
         gameObject.SetActive(isActive);
+        _UI.SetActive(isActive);
     }
 }
