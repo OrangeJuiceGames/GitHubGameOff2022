1b1a13e9f58e907a1ae5a1852e0bea9eb89f83c5
Juan Erenas <juancerenas@gmail.com>
2022-11-17 23:45:07 -0800

    Mob shot impulse added.

--- Assets/Code/Enemy/Mobs/Mob.cs
+++ Assets/Code/Enemy/Mobs/Mob.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Mob : MonoBehaviour
 {
+
+    private Vector3 _impulseForce = new Vector3(0, 20f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +18,27 @@ public class Mob : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Shot(Clone)")
+        {
+            AddUpwardsImpulse();
+        }
+        else if (collision.gameObject.name == "Floor")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void AddUpwardsImpulse()
+    {
+        GetComponent<Rigidbody2D>().AddForce(_impulseForce, ForceMode2D.Impulse);
+    }
+
+
+
+
+
+
 }
