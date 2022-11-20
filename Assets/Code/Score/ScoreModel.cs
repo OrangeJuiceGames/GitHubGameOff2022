ba4a4326f547bc0477467581db5b0aeef0f3f692
Brandon Khan <38743905+wtmk-dev@users.noreply.github.com>
2022-11-20 14:47:06 -0800

    score system ui

---
+++ Assets/Code/Score/ScoreModel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScoreModel
+{
+    public float InvasionScore { get; set; }
+    public int DogsDead { get; set; }
+    public long Score { get; set; }
+    public float Time { get; set; }
+}
--- Assets/Code/Score/ScoreSystem.cs
+++ Assets/Code/Score/ScoreSystem.cs
@@ -8,7 +8,22 @@ public class ScoreSystem
     public ScoreSystem(Stage stage)
     {
         _Stage = stage;
+        _Stage.Floor.OnDogKilled += DogKilled;
+        _Stage.Floor.OnInvasionIncrease += InvasionIncreased;
     }
 
+    private void DogKilled(int value)
+    {
+        Debug.Log(value);
+    }
 
+    private void InvasionIncreased(int value)
+    {
+        Debug.Log(value);
+    }
+
+    private void MobSaved(MobType type)
+    {
+        Debug.Log(type);
+    }
 }
